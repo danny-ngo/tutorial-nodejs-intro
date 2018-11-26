730cb97bb0f007ec38889d420755edf384f27026
austinoboyle <14arob@queensu.ca>
2018-11-26 10:51:15 -0500

    Refactor registration into 1 route with 2 methods

--- app/routes.js
+++ app/routes.js
@@ -6,29 +6,34 @@ module.exports = function(app, passport) {
         res.render("login");
     });
 
-    app.post("/users", (req, res, next) => {
-        const { username, password } = req.body;
-        return bcrypt.hash(password, bcrypt.genSaltSync(8), null, function(
-            err,
-            hashed
-        ) {
-            if (err) {
-                throw err;
-            }
-            return User.create({ username, password: hashed })
-                .then(user => {
-                    res.json({ success: true, user });
-                })
-                .catch(e => next(e));
+    app.route("/signup")
+        .get((req, res) => {
+            res.render("signup");
+        })
+        .post((req, res, next) => {
+            const { username, password } = req.body;
+            return bcrypt.hash(password, bcrypt.genSaltSync(8), null, function(
+                err,
+                hashed
+            ) {
+                if (err) {
+                    throw err;
+                }
+                return User.create({ username, password: hashed })
+                    .then(user => {
+                        res.json({ success: true, user });
+                    })
+                    .catch(e => next(e));
+            });
         });
-    });
 
     app.post("/login", passport.authenticate("local"), (req, res, next) => {
         res.json({ user: req.user });
     });
 
-    app.get("/signup", function(req, res) {
-        res.render("signup");
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
     });
 
     app.use((req, res, next) => {
