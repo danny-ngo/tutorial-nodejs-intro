989ab373010ffd780c07af46b3c8b41957e332d3
austinoboyle <14arob@queensu.ca>
2018-11-28 00:23:37 -0500

    Adjust routes to properly use passport auth

--- app/routes.js
+++ app/routes.js
@@ -1,39 +1,47 @@
 const { User } = require("../db");
 const bcrypt = require("bcrypt-nodejs");
+const { isAuthenticated } = require("./middleware");
 
 module.exports = function(app, passport) {
-    app.get("/", function(req, res) {
-        res.render("login");
+    app.get("/", isAuthenticated, (req, res, next) => {
+        res.render("index", { user: req.user.toJSON() });
     });
 
     app.route("/signup")
-        .get((req, res) => {
-            res.render("signup");
+        .get(function(req, res) {
+            if (req.isAuthenticated()) {
+                res.redirect("/");
+            }
+            // render the page and pass in any flash data if it exists
+            res.render("signup", { message: req.flash("signupMessage") });
         })
-        .post((req, res, next) => {
-            const { username, password } = req.body;
-            return bcrypt.hash(password, bcrypt.genSaltSync(8), null, function(
-                err,
-                hashed
-            ) {
-                if (err) {
-                    throw err;
-                }
-                return User.create({ username, password: hashed })
-                    .then(user => {
-                        res.render("index", { user });
-                    })
-                    .catch(e => next(e));
-            });
-        });
+        .post(
+            passport.authenticate("signup", {
+                successRedirect: "/",
+                failureFlash: true,
+                failureRedirect: "/signup"
+            })
+        );
 
-    app.post("/login", passport.authenticate("local"), (req, res, next) => {
-        res.render("index", { user: req.user });
-    });
+    app.route("/login")
+        .get((req, res, next) => {
+            if (req.isAuthenticated()) {
+                res.redirect("/");
+            } else {
+                res.render("login", { message: req.flash("loginMessage") });
+            }
+        })
+        .post(
+            passport.authenticate("login", {
+                successRedirect: "/", // redirect to the secure profile section
+                failureRedirect: "/login", // redirect back to the signup page if there is an error
+                failureFlash: true // allow flash messages
+            })
+        );
 
     app.get("/logout", function(req, res) {
         req.logout();
-        res.redirect("/");
+        res.redirect("/login");
     });
 
     app.use((req, res, next) => {
@@ -42,7 +50,7 @@ module.exports = function(app, passport) {
     });
 
     app.use((err, req, res, next) => {
-        console.error(err);
+        console.error(err.name);
         res.status(err.status || 500).json({ message: err.message });
     });
 };
